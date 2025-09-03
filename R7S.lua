ff121f87d829250fd7074ed7f24ac52bb2000ec2
HyRexxyy <m.adiwijaya28@gmail.com>
2025-09-03 07:18:01 +0700

    R7S.lua

---
+++ R7S.lua
@@ -0,0 +1,25 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+local Workspace = game:GetService("Workspace")
+local LocalPlayer = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+
+local Window = Rayfield:CreateWindow({
+   Name = "Rayfield Example Window
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ShowText = "Rayfield",
+   Theme = "Default",
+   ToggleUIKeybind = "K",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Big Hub"
+   KeySystem = false,
+   }
+})
